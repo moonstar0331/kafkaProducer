af052ea2dafe6028ecbeaa92b020b84d7644631c
MoonStar <moonsung0331@gmail.com>
2023-08-19 00:09:47 +0900

    간단한 pub/sub 테스트를 위한 코드 작성

---
+++ src/main/java/com/example/kafkaproducer/service/Producer.java
@@ -0,0 +1,23 @@
+package com.example.kafkaproducer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Producer {
+
+    // Publishing 할 Topic
+    String topicName = "studyKafka";
+
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Autowired
+    public Producer(KafkaTemplate kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void pub(String msg) {
+        kafkaTemplate.send(topicName, msg);
+    }
+}
