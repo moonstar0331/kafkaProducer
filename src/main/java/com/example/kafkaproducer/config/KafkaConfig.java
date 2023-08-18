d275775323dcc22b2fd596d4007abaf23cdf3f0f
MoonStar <moonsung0331@gmail.com>
2023-08-18 18:15:34 +0900

    Kafka 사용을 위한 Configuration 작성
    Producer 는 ProducerFactory 를 통해 KafkaTemplate 를 이용해서 데이터를 publishing 하게 될 것이다.

---
+++ src/main/java/com/example/kafkaproducer/config/KafkaConfig.java
@@ -0,0 +1,33 @@
+package com.example.kafkaproducer.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<String, Object>(producerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> myConfig = new HashMap<>();
+        myConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "43.201.66.101:9092, 43.201.101.8:9092, 43.200.177.54:9092");
+        myConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        myConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(myConfig);
+    }
+}
