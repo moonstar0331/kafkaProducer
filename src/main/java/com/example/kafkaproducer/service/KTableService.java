49d16557b4ead350fce2770e4b1236a69fa7f5b9
MoonStar <moonsung0331@gmail.com>
2023-08-19 18:45:50 +0900

    사용하지 않는 코드 주석처리

--- src/main/java/com/example/kafkaproducer/service/KTableService.java
+++ src/main/java/com/example/kafkaproducer/service/KTableService.java
@@ -11,16 +11,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class KTableService {
 
-    @Autowired
-    public void buildPipeline(StreamsBuilder sb) {
-        KTable<String, String> leftTable = sb.stream("leftTopic", Consumed.with(Serdes.String(), Serdes.String())).toTable();
-        KTable<String, String> rightTable = sb.stream("rightTopic", Consumed.with(Serdes.String(), Serdes.String())).toTable();
-
-        ValueJoiner<String, String, String> stringJoiner = (leftValue, rightValue) -> {
-            return "[StringJoiner]" + leftValue + "-" + rightValue;
-        };
-
-        KTable<String, String> joinedTable = leftTable.join(rightTable, stringJoiner);
-        joinedTable.toStream().to("joinedMsg");
-    }
+//    @Autowired
+//    public void buildPipeline(StreamsBuilder sb) {
+//        KTable<String, String> leftTable = sb.stream("leftTopic", Consumed.with(Serdes.String(), Serdes.String())).toTable();
+//        KTable<String, String> rightTable = sb.stream("rightTopic", Consumed.with(Serdes.String(), Serdes.String())).toTable();
+//
+//        ValueJoiner<String, String, String> stringJoiner = (leftValue, rightValue) -> {
+//            return "[StringJoiner]" + leftValue + "-" + rightValue;
+//        };
+//
+//        KTable<String, String> joinedTable = leftTable.join(rightTable, stringJoiner);
+//        joinedTable.toStream().to("joinedMsg");
+//    }
 }
