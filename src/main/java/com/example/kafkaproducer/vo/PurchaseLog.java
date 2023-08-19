e505e9157b4df587adebf892b7fad057a5c689b3
MoonStar <moonsung0331@gmail.com>
2023-08-19 18:46:22 +0900

    광고-구매 Join을 통한 광고효과 Streaming 에 입력한 데이터 vo 속성 수정 및 추가

--- src/main/java/com/example/kafkaproducer/vo/PurchaseLog.java
+++ src/main/java/com/example/kafkaproducer/vo/PurchaseLog.java
@@ -2,12 +2,15 @@ package com.example.kafkaproducer.vo;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+
 @Data
 public class PurchaseLog { // 구매한 이력
 
+    String orderId; // od-0001
     String userId; // uid-0001
-    String productId; // pg-0001
-    String purchaseDt; // 20230819070000
+    ArrayList<String> productId; // pg-0001
+    String purchasedDt; // 20230819070000
     Long price; // 24000
 
 }
--- src/main/java/com/example/kafkaproducer/vo/WatchingAdLog.java
+++ src/main/java/com/example/kafkaproducer/vo/WatchingAdLog.java
@@ -9,5 +9,6 @@ public class WatchingAdLog { // 광고를 본 이력
     String productId; // pg-0001
     String adId; // ad-101
     String adType; // banner, clip,  main, live
+    String watchingTime; // 머문 시간
     String watchingDt; // 20230819090000
 }
