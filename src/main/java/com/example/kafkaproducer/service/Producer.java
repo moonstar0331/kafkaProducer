40791048c98205d233e0bd5af78d41fb48382ad4
MoonStar <moonsung0331@gmail.com>
2023-08-20 14:57:43 +0900

    생성된 message 를 각 VO에 맞게 Producing 하기 위한 Producer 메소드 작성

--- src/main/java/com/example/kafkaproducer/service/Producer.java
+++ src/main/java/com/example/kafkaproducer/service/Producer.java
@@ -1,5 +1,6 @@
 package com.example.kafkaproducer.service;
 
+import com.example.kafkaproducer.config.KafkaConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -7,21 +8,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class Producer {
 
+    @Autowired
+    KafkaConfig myConfig;
+
     // Publishing 할 Topic
-    String topicName = "studyKafka";
+    String topicName = "defaultTopic";
 
     private KafkaTemplate<String, Object> kafkaTemplate;
 
-    @Autowired
-    public Producer(KafkaTemplate kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
+//    @Autowired
+//    public Producer(KafkaTemplate kafkaTemplate) {
+//        this.kafkaTemplate = kafkaTemplate;
+//    }
 
     public void pub(String msg) {
+        kafkaTemplate = myConfig.kafkaTemplateForGeneral();
         kafkaTemplate.send(topicName, msg);
     }
 
     public void sendJoinedMsg(String topicNm, Object msg) {
+        kafkaTemplate = myConfig.kafkaTemplateForGeneral();
+        kafkaTemplate.send(topicNm, msg);
+    }
+
+    public void sendMsgForWatchingAdLog(String topicNm, Object msg) {
+        kafkaTemplate = myConfig.kafkaTemplateForWatchingAdLog();
+        kafkaTemplate.send(topicNm, msg);
+    }
+
+    public void sendMsgForPurchaseLog(String topicNm, Object msg) {
+        kafkaTemplate = myConfig.kafkaTemplateForPurchaseLog();
         kafkaTemplate.send(topicNm, msg);
     }
 }
